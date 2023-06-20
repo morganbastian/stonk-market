9b04ea1b011cd8d9ef298cc424aea92f45e8ce07
Morgan Bastian <morganbastian07@gmail.com>
2023-06-20 18:10:38 -0400

    create stockinfo view and route for view

---
+++ src/views/StockInfo.js
@@ -0,0 +1,16 @@
+//** Import Statements
+
+//** Setup (define helper functions and variables here)
+
+function StockInfo(props) {
+	//** Destructure Props
+	const {} = props;
+
+	//** State Variables
+
+	//** Component Logic
+
+	//** Return JSX
+	return <div>{/*replace with your JSX Here */}</div>;
+}
+export default StockInfo;
