dd65a87dc6feaa4dcf372eaa6611f7207cdd23f6
Morgan Bastian <morganbastian07@gmail.com>
2023-06-19 22:47:09 -0400

    init msw mui and react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const prepare = () => {
+	if (process.env.NODE_ENV === 'development') {
+		// prevents the mock server from working in a deployed, production env
+		const { worker } = require('./mocks/browser.js');
+		return worker.start(); // Comment out this return statement to disable the mock api
+	}
+	return Promise.resolve();
+};
+
+prepare().then(() => {
+	const root = ReactDOM.createRoot(document.getElementById('root'));
+	root.render(
+		<React.StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</React.StrictMode>
+	);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
