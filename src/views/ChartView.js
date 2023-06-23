81d78f838f950111afcb2e563eb3d4facf0406e9
Morgan Bastian <morganbastian07@gmail.com>
2023-06-23 02:36:37 -0400

    unsure of error

---
+++ src/views/ChartView.js
@@ -0,0 +1,21 @@
+//** Import Statements
+
+import Chart from '../components/Chart';
+//** Setup (define helper functions and variables here)
+
+function ChartView(props) {
+	//** Destructure Props
+	const {} = props;
+
+	//** State Variables
+
+	//** Component Logic
+
+	//** Return JSX
+	return (
+		<div>
+			<Chart />
+		</div>
+	);
+}
+export default ChartView;
